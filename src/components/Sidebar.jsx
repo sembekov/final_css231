72b2e3f58bb8bc062e7fd5415131a0419fa8b43d
Darkhan <darxghostkz@gmail.com>
2024-12-21 15:36:59 +0500

    adaptive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import './Sidebar.css';
-import { FaUser } from "react-icons/fa";
-import { FaRegListAlt } from "react-icons/fa";
+import { FaBars, FaUser } from "react-icons/fa";
 import { RiLogoutBoxLine } from "react-icons/ri";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false); 
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); 
+  };
+
   return (
-    <div className="sidebar">
-      <h3>Profile Menu</h3>
-      <ul>
-        <li>
-          <FaUser className="icon" />
-          <NavLink to="/profile" activeClassName="active">Profile</NavLink>
-        </li>
-        <li>
-          <FaRegListAlt className="icon" />
-          <NavLink to="/your-orders" activeClassName="active">Your Orders</NavLink>
-        </li>
-        <li>
-          <RiLogoutBoxLine className="icon" />
-          <NavLink to="/" activeClassName="active">Sign Out</NavLink>
-        </li>
-      </ul>
-    </div>
+    <>
+      {}
+      <button className="sidebar-toggle" onClick={toggleSidebar}>
+        <FaBars />
+      </button>
+
+      {}
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+        <h3>Profile Menu</h3>
+        <ul>
+          <li>
+            <FaUser className="icon" />
+            <NavLink to="/profile" activeClassName="active">Profile</NavLink>
+          </li>
+          <li>
+            <RiLogoutBoxLine className="icon" />
+            <NavLink to="/home" activeClassName="active">Sign Out</NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
